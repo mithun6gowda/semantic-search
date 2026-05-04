1dace852e9cb265866adebd777fa4b5659f9ff69
mithun6gowda <mithungowdaparamesh@gmail.com>
2026-05-04 22:52:29 +0530

    Refactor embedding functions to use SentenceTransformer for improved performance

--- app/embedding.py
+++ app/embedding.py
@@ -1,15 +1,23 @@
-from openai import OpenAI
-from app.config import OPENAI_API_KEY
+# from openai import OpenAI
+# from app.config import OPENAI_API_KEY
 
-def get_client():
-    return OpenAI(api_key=OPENAI_API_KEY)
+# def get_client():
+#     return OpenAI(api_key=OPENAI_API_KEY)
+
+# def get_embedding(text: str):
+#     client = get_client()
+
+#     response = client.embeddings.create(
+#         model="text-embedding-3-small",
+#         input=text
+#     )
+
+#     return response.data[0].embedding
+
+
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_embedding(text: str):
-    client = get_client()
-
-    response = client.embeddings.create(
-        model="text-embedding-3-small",
-        input=text
-    )
-
-    return response.data[0].embedding
+    return model.encode(text).tolist()
